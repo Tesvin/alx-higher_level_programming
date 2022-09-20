90500c50804e9010f5fab22fc4698762698d7f36
Tesvin <57831060+Tesvin@users.noreply.github.com>
2022-09-21 00:09:33 +0100

    Update 9-print_last_digit.py
    commit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -3,7 +3,7 @@ def print_last_digit(number):
     if number > 0:
         number = number % 10
     else:
-        aux number * -1
+        aux = number * -1
         number = aux % 10
     print(number, end="")
     return number
