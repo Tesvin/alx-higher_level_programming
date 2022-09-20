3c03e922e25ed0759c5ea1a4ef9ba3c3706faa97
Tesvin <57831060+Tesvin@users.noreply.github.com>
2022-09-21 00:34:39 +0100

    Update 100-print_tebahpla.py
    commit

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -5,10 +5,3 @@ for i in range(-122, -96):
     else:
         let = chr((i * -1) - 32)
     print("{}".format(let), end="")
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
