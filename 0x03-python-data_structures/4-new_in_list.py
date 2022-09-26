08f491e92b58d7ed95177a0703617c596588aaf1
tesvin <stephenazosike@gmail.com>
2022-09-26 07:53:39 -0700

    commit

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 def new_in_list(my_list, idx, element):
     copy = my_list.copy()
     for i in my_list:
-        if idx == my_list.index():
+        if idx == my_list.index(i):
             element = copy[idx]
             return (copy)
         elif idx >= len(my_list):
