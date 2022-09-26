3a5b4f876239fcbfef094d38571867d8541e6853
Tesvin <57831060+Tesvin@users.noreply.github.com>
2022-09-26 22:53:20 +0100

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def no_c(my_string):
     new = list(my_string)
-    for i in new:
-        if i == 'c':
+    for x in new:
+        if x == 'c':
             new.remove('c')
-        if i == 'C':
+        if x == 'C':
             new.remove('C')
     my_string = "".join(new)
     return my_string
