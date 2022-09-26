0d5f8fc77ca1860172c432e75376f2e9c5bc149b
tesvin <stephenazosike@gmail.com>
2022-09-26 14:57:42 -0700

    commit

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    l = list(my_string)
-    for x in l:
+    new = list(my_string)
+    for x in new:
         if x == 'c':
-            l.remove('c')
+            new.remove('c')
         if x == 'C':
-            l.remove('C')
-    my_string = "".join(l)
+            new.remove('C')
+    my_string = "".join(new)
     return my_string
