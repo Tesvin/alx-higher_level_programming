9c1f1ff928e9bc5beb00914fa39048be53b8f50f
tesvin <stephenazosike@gmail.com>
2022-09-28 15:10:24 -0700

    commit

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    result = sum(set(my_list))
-    print("Result: {:d}".format(result))
+    sum = 0
+    for i in set(my_list):
+        sum += i
+    return sum
