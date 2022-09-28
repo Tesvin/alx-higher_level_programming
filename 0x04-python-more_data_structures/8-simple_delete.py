43d18d8e5dbb928ba431ab6346ee8e4d5e713511
tesvin <stephenazosike@gmail.com>
2022-09-28 16:34:50 -0700

    commit

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
     """function that deletes a key in a dictionary."""
-    a_dictionary.pop[key]
+    for i in a_dictionary:
+        del a_dictionary(key)
     return a_dictionary
