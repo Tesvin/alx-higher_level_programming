4f80763115362fe5986e3d9c95c425d86bddf5a2
tesvin <stephenazosike@gmail.com>
2022-09-28 17:49:40 -0700

    commit

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    new_dict = sorted(a_dictionary)
-    for key in new_dict:
-        if key =< len(new_dict) - 1:
-            return None
-        else:
-            return key
+    """function that returns a key with the biggest integer value."""
+    if not a_dictionary:
+        return None
+    return max(a_dictionary, key=a_dictionary.get())
