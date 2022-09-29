1b933e35ef09a1a4a0c618ab75bb1e601bb50ea1
tesvin <stephenazosike@gmail.com>
2022-09-28 18:34:11 -0700

    commit

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,4 +4,4 @@ def square_matrix_simple(matrix=[]):
     if not matrix:
         return None
 
-    return list(list(map(lambda x: x*x, new_mat)) for new_mat in matrix)
+    return list(list(map(lambda x: x*x, num_list)) for num_list in matrix)
