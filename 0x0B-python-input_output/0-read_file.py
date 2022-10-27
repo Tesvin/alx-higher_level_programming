bd2482ec78a2044c2e9272ba3cf1cb27793040c9
tesvin <stephenazosike@gmail.com>
2022-10-26 18:48:50 -0700

    commit

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,10 +3,10 @@
 function that reads a text file
 """
 
+
 def read_file(filename=""):
-    """
-    reads a text file (UTF8) and prints it to stdout
+    """reads a text file (UTF8) and prints it to stdout
     Returns none
     """
-    with open('work', 'r', encoding="utf-8") as f:
+    with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
